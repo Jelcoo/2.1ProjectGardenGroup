b086ef84f9f37fe0c727436df85f90f353a0d40a
Asiel <143839022+AsielElaouare@users.noreply.github.com>
2024-10-09 08:06:46 +0200

    first commit

---
+++ Logic/DashboardLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Threading.Tasks;
+using LiveChartsCore.SkiaSharpView.Extensions;
+
+namespace Logic
+{
+    internal class DashboardLogic : ObservableObject
+    {
+
+        public IEnumerable<ISeries> Series { get; set; } =
+        new[] { 2, 4, 1, 4, 3 }.AsPieSeries((value, series) =>
+        {
+            series.MaxRadialColumnWidth = 60;
+        });
+    }
+}
