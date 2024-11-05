6b7a059ccfaeebff1d02a64b64eb3e14bb7b6220
Natfaji_A <modemoon2003@hotmail.com>
2024-11-05 21:52:21 +0100

    remove unused refrences fix some formating issues

--- Logic/TicketLogic.cs
+++ Logic/TicketLogic.cs
@@ -2,12 +2,6 @@
 using Model.Enums;
 using Model.Models;
 using MongoDB.Bson;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Logic
 {
 	public class TicketLogic
